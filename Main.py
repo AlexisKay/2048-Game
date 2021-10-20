1b7ff070148a1bef685a26b491ad777d0f28933a
Alexis McBeain <alexiskay1998@gmail.com>
2021-10-20 12:33:16 -0600

    update some code to main

--- Main.py
+++ Main.py
@@ -2,3 +2,41 @@ from tkinter import *
 from tkinter import messagebox
 import random
 
+class board:
+    background_color={
+        '2':'',
+        '4':'',
+        '8':'',
+        '16':'',
+        '32':'',
+        '64':'',
+        '128':'',
+        '256':'',
+        '512':'',
+        '1024':'',
+        '2048':'',
+    }
+
+    foreground_color={
+          '2':'',
+        '4':'',
+        '8':'',
+        '16':'',
+        '32':'',
+        '64':'',
+        '128':'',
+        '256':'',
+        '512':'',
+        '1024':'',
+        '2048':'',
+    }
+
+    def __init__(self) -> None:
+        self.n = 4
+        self.window.Tk()
+        self.window.title("2048 by AlexisKay")
+        self.gameArea=Frame(self.window,background=('azure3'))
+        self.board=[]
+
+
+        pass
